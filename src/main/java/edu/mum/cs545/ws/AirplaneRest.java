a797abfc695390bb189be98f9c4d262f8eb95b87
Nguyen Tran <tranvinhnguyen@gmail.com>
2017-06-10 08:28:16 -0500

    release for rest services

---
+++ src/main/java/edu/mum/cs545/ws/AirplaneRest.java
@@ -0,0 +1,86 @@
+package edu.mum.cs545.ws;
+
+
+
+import java.util.List;
+
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+
+import cs545.airline.model.Airplane;
+import cs545.airline.service.AirplaneService;
+
+
+@Named
+@Path("airplane")
+public class AirplaneRest {
+
+	@Inject
+	private AirplaneService airlineService;
+
+	@Path("findall")
+	@GET
+	public List<Airplane> findAll() {
+		return airlineService.findAll();
+	}
+
+	@Path("findbymodel/{model}")
+	@GET
+	public List<Airplane> findByModel(@PathParam("model") String model) {
+		return airlineService.findByModel(model);
+	}
+
+	@Path("findbyserial/{serial}")
+	@GET
+	public Airplane findBySerial(@PathParam("serial") String serial) {
+		return airlineService.findBySrlnr(serial);
+	}
+
+	@Path("findbyflight/{flightid}")
+	@GET
+	public List<Airplane> findByFlight(@PathParam("flightid") long flightid) {
+		return airlineService.findByFlight(flightid);
+	}
+
+	@Path("create")
+	@POST
+	public String create(Airplane airplane) {
+		try {
+			airlineService.create(airplane);
+		} catch (Exception e) {
+			return "Fail to create this object";
+		}
+		return "create airplane successfully";	
+		
+	}
+	
+	@Path("delete")
+	@DELETE
+	public String delete(Airplane airplane) {
+		try {
+			airlineService.delete(airplane);
+		} catch (Exception e) {
+			return "Fail to delete this object";
+		}
+		return "deleted airplane successfully";	
+	}
+	
+	@Path("update")
+	@PUT
+	public String update(Airplane airplane) {
+		try {
+			airlineService.update(airplane);
+		} catch (Exception e) {
+			return "Fail to update this object";
+		}
+		return "Updated airplane successfully";	
+	}
+}
---
+++ src/main/java/edu/mum/cs545/ws/AirportRest.java
@@ -0,0 +1,101 @@
+package edu.mum.cs545.ws;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import cs545.airline.model.Airport;
+import cs545.airline.model.Flight;
+import cs545.airline.service.AirportService;
+
+@Path("airport")
+public class AirportRest {
+	
+	@Inject
+	private AirportService airportService;
+	
+	
+	@Path("create")
+	@POST
+	public String create(Airport airport) {
+		try {
+			airportService.create(airport);
+		} catch (Exception e) {
+			return "Fail to create this object";
+		}
+		return "create Airport successfully";	
+		
+
+	}
+	
+	@Path("delete")
+	@DELETE
+	public String delete(Airport airport) {
+		try {
+			airportService.delete(airport);
+		} catch (Exception e) {
+			return "Fail to delete this object";
+		}
+		return "deleted airport successfully";	
+	}
+	
+	@Path("update")
+	@PUT
+	public String update(Airport airport) {
+		try {
+			airportService.update(airport);
+		} catch (Exception e) {
+			return "Fail to update this object";
+		}
+		return "Updated airplane successfully";	
+	}
+	
+	@Path("findall")
+	@GET
+	public List<Airport> findAll() {
+		return airportService.findAll();
+	}
+	
+	@Path("findbycity/{city}")
+	@GET
+	public List<Airport> findByModel(@PathParam("city") String city) {
+		return airportService.findByCity(city);
+	}
+	
+	@Path("findbyarrival")
+	@GET
+	public List<Airport> findByArrival(Flight flight) {
+		return airportService.findByArrival(flight);
+	}
+	
+	
+	@Path("findbycode/{code}")
+	@GET
+	public Airport findByCode(@PathParam("code") String code) {
+		return airportService.findByCode(code);
+	}
+	
+	@Path("findbycountry/{country}")
+	@GET
+	public List<Airport> findByCountry(@PathParam("country") String country) {
+		return airportService.findByCountry(country);
+	}
+	
+	@Path("findbydeparture")
+	@GET
+	public List<Airport> findByDeparture(Flight flight) {
+		return airportService.findByDeparture(flight);
+	}
+	
+	@Path("findbyname/{name}")
+	@GET
+	public List<Airport> findByName(@PathParam("flight") String name) {
+		return airportService.findByName(name);
+	}
+	
+}
