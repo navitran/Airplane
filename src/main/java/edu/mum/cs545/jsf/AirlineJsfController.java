d968119f2052eed7d0c78d654bc130791cf1fd8e
Nguyen Tran <tranvinhnguyen@gmail.com>
2017-06-11 08:39:50 -0500

    add view JSF for flight and airline

--- src/main/java/edu/mum/cs545/jsf/AirlineJsfController.java
+++ src/main/java/edu/mum/cs545/jsf/AirlineJsfController.java
@@ -1,5 +1,82 @@
 package edu.mum.cs545.jsf;
 
-public class AirlineJsfController {
+import java.io.Serializable;
+import java.util.List;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import cs545.airline.model.Airline;
+import cs545.airline.service.AirlineService;
+
+@Named
+@SessionScoped
+public class AirlineJsfController implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	// private static AirlineService airlineService = new AirlineService();
+	@Inject
+	private AirlineService airlineService;
+
+	private List<Airline> listOfAirlines;
+	private Airline airline;
+
+	public List<Airline> getListOfAirlines() {
+		return airlineService.findAll();
+	}
+
+	public void setListOfAirlines(List<Airline> listOfAirlines) {
+		this.listOfAirlines = listOfAirlines;
+	}
+
+	public Airline getAirline() {
+		return airline;
+	}
+
+	public void setAirline(Airline airline) {
+		this.airline = airline;
+	}
+
+	public String airlinedetail() {
+		airline = airlineService.findByName("SkyTeam");
+		return "airlinedetail";
+	}
+
+	public String create() {
+
+		try {
+			airline = new Airline(airline.getName());
+			airlineService.create(airline);
+		} catch (Exception e) {
+			return "createairline";
+		}
+		return "listallairlines";
+
+	}
+
+	public String update() {
+
+		try {
+			airlineService.update(airline);
+		} catch (Exception e) {
+			return "updateairline";
+		}
+		return "listallairlines";
+
+	}
+
+	public String delete() {
+
+		try {
+			
+			airlineService.delete(airline);
+		} catch (Exception e) {
+			return "deleteairline";
+		}
+		return "listallairlines";
+
+	}
 
 }
--- src/main/java/edu/mum/cs545/jsf/FlightJsfController.java
+++ src/main/java/edu/mum/cs545/jsf/FlightJsfController.java
@@ -1,16 +1,37 @@
 package edu.mum.cs545.jsf;
 
+import java.util.Collection;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import cs545.airline.model.Flight;
 import cs545.airline.service.FlightService;
+
 
 @Named
 @ApplicationScoped
 public class FlightJsfController {
 	@Inject
 	private FlightService flightService;
+	
+	private Collection<Flight> flights;
+
+	public Collection<Flight> getAccounts() {
+		return flightService.findAll();
+	}
+
+	public Collection<Flight> getFlights() {
+		return flights;
+	}
+
+	public void setFlights(Collection<Flight> flights) {
+		this.flights = flights;
+	}
+
+
+	
 	public String  findAll(){
 		
 		
