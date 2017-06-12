24ef49ac0e1c41288dcc46cb7b43c50cf43bc665
Nguyen Tran <tranvinhnguyen@gmail.com>
2017-06-11 23:35:08 -0500

    implement filter flight by airline

--- src/main/java/edu/mum/cs545/jsf/FlightJsfController.java
+++ src/main/java/edu/mum/cs545/jsf/FlightJsfController.java
@@ -1,19 +1,19 @@
 package edu.mum.cs545.jsf;
 
-
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import cs545.airline.model.Airline;
 import cs545.airline.model.Flight;
 import cs545.airline.service.FlightService;
 
 @Named
 @ApplicationScoped
 public class FlightJsfController {
-	
+
 	@Inject
 	private FlightService flightService;
 
@@ -27,6 +27,15 @@ public class FlightJsfController {
 		this.listOfFlights = listOfFlights;
 	}
 
+	public String findByAirline(String name) {
+		try {
+			Airline airline = new Airline(name);
+			this.listOfFlights = flightService.findByAirline(airline);
+		} catch (Exception e) {
+			return "flight";
+		}
+		return "flight";
 
+	}
 
 }
